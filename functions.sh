b2c5d917ed68d9376b349380335d3f0775d3f92c
Nic Patterson <nic.patterson@sap.com>
2018-11-19 15:34:29 -0800

    helm and kube image variables

--- functions.sh
+++ functions.sh
@@ -1,11 +1,13 @@
 function kube() {
   # .kube contains the kubernetes config files
-  docker run --rm -v "$(pwd):/kube" -v "$HOME/.kube:/root/.kube" -w /kube quay.cnqr.delivery/containerhosting/kubectl "$@"
+  kube_image=${KUBECTL_IMAGE:-quay.cnqr.delivery/containerhosting/kubectl}
+  docker run --rm -v "$(pwd):/kube" -v "$HOME/.kube:/root/.kube" -w /kube ${kube_image} "$@"
 }
 
 function helm() {
   # .appr is for HELM application registry
   # .helm contains the helm configs
   # .kube contains the kubernetes config files
-  docker run --rm -v "$(pwd):/helm" -v "$HOME/.appr:/root/.appr" -v "$HOME/.helm:/root/.helm" -v "$HOME/.kube:/root/.kube" -w /helm quay.cnqr.delivery/containerhosting/helm-client:v2.7.2-appr-template "$@"
+  helm_image=${HELM_IMAGE:-quay.cnqr.delivery/containerhosting/helm-client:v2.7.2-appr-template}
+  docker run --rm -v "$(pwd):/helm" -v "$HOME/.appr:/root/.appr" -v "$HOME/.helm:/root/.helm" -v "$HOME/.kube:/root/.kube" -w /helm $helm_image "$@"
 }
