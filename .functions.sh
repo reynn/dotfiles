fed9c6a80bcf381fd5c5c662408b905456d2a291
Nic Patterson <arasureynn@gmail.com>
2019-04-19 10:37:46 -0700

    include more funcs

--- .functions.sh
+++ .functions.sh
@@ -4,6 +4,19 @@ function kube() {
   docker run --rm -v "$(pwd):/kube" -v "$HOME/.kube:/root/.kube" -w /kube ${kube_image} "$@"
 }
 
+function listenport() {
+  PORT=$1
+  lsof -nP -i4TCP:$PORT | grep LISTEN | awk '{ print $2 }'
+}
+
+function killlistening() {
+  PORT=$1
+  PROCESS=$(listenport $PORT)
+
+  echo "Killing process $PROCESS"
+  sudo kill -9 $PROCESS
+}
+
 function helm() {
   # .appr is for HELM application registry
   # .helm contains the helm configs
