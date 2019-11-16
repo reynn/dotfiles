41268ab94eaba89edce64c732adfde99453c6173
Nic Patterson <224374+reynn@users.noreply.github.com>
2019-11-15 18:31:14 -0800

    finish daily yt script

--- scripts/youtube-daily.sh
+++ scripts/youtube-daily.sh
@@ -5,20 +5,20 @@ CHANNEL_LIST="${2:-$BASE_PATH/channel_list.txt}"
 ARCHIVE_LOG="${3:-$BASE_PATH/downloaded.txt}"
 SINCE="${4:-`date -d "28 hours ago" '+%Y%m%d'`}"
 
-# youtube-dl \
-#   --playlist-reverse \
-#   --download-archive "$ARCHIVE_LOG" \
-#   -i \
-#   -o "$BASE_PATH/%(uploader)s [%(channel_id)s]/%(uploader)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" \
-#   -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' \
-#   --merge-output-format mp4 \
-#   --add-metadata \
-#   --no-continue \
-#   --no-overwrites \
-#   --write-thumbnail \
-#   --embed-thumbnail \
-#   --all-subs \
-#   --sub-format "srt" \
-#   --embed-subs \
-#   --dateafter "$(date +%Y)0101" \
-#   --batch-file="$CHANNEL_LIST"
+youtube-dl \
+  --playlist-reverse \
+  --download-archive "$ARCHIVE_LOG" \
+  -i \
+  -o "$BASE_PATH/%(uploader)s [%(channel_id)s]/%(uploader)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" \
+  -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' \
+  --merge-output-format mp4 \
+  --add-metadata \
+  --no-continue \
+  --no-overwrites \
+  --write-thumbnail \
+  --embed-thumbnail \
+  --all-subs \
+  --sub-format "srt" \
+  --embed-subs \
+  --dateafter "$SINCE" \
+  --batch-file="$CHANNEL_LIST"
