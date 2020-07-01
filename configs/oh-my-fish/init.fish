c889d6c473151f8dc26378200f859c46b50e2294
Nic Patterson <nic@reynn.dev>
2020-07-01 10:40:15 -0700

    use list to source extra files

--- configs/oh-my-fish/init.fish
+++ configs/oh-my-fish/init.fish
@@ -1,6 +1,10 @@
-source $OMF_CONFIG/exports.fish
-source $OMF_CONFIG/aliases.fish
-source $OMF_CONFIG/functions.fish
+set sourceables exports aliases functions
+
+for sourceable in $sourceables
+    if test -e $OMF_CONFIG/$sourceable.fish
+        source $OMF_CONFIG/$sourceable.fish
+    end
+end
 
 ## Source additional files
 source "$GFP/github.com/junegunn/fzf/shell/key-bindings.fish"
