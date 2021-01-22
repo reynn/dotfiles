8398af417abcdb8b06d186fcbeb3654f1c7f28ac
Nic Patterson <nic@reynn.dev>
2021-01-22 05:56:02 +0000

    placeholder funcs

---
+++ configs/fish/functions/dotfiles.fisher.update.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+function dotfiles.fisher.update -d "Run updates for fisher"
+	log.info "Updating fisher"
+end
---
+++ configs/fish/functions/dotfiles.links.update.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+function dotfiles.links.update -d "Run updates for links"
+	log.info "Updating links"
+end
