fcb090bbbad2445081272a508cafddefd0559c75
Nic Patterson <nic.patterson@sap.com>
2022-07-15 14:58:50 -0700

    add alacritty kanagawa color scheme, update git.clone to allow recursive clone

--- configs/fish/functions/git.clone.fish
+++ configs/fish/functions/git.clone.fish
@@ -9,14 +9,18 @@ function git.clone -d "Clone repository to the layout used"
     set -x protocol ssh
     set -x repos
     set -x cd_into
+    set -x recursive
+    set -x full_checkout false
 
     function ___usage
         set -l help_args -a "Clone repository to the layout used [format:'$base_directory/$git_host/{owner}/{repo}']"
-        set -a help_args -f 'r|repos|The owner/name of the repository to clone'
         set -a help_args -f "b|base-dir|The base directory to store repositories|$base_directory"
-        set -a help_args -f "p|protocol|Clone protocol, either HTTPS or SSH|$protocol"
-        set -a help_args -f "H|host|GitHub host name, for using against enterprise GitHub|$git_host"
         set -a help_args -f "c|cd|`cd` into the directory after clone|$cd_into"
+        set -a help_args -f "F|full-checkout|Does a complete checkout of the repo instead of shallow clone|$full_checkout"
+        set -a help_args -f "H|host|GitHub host name, for using against enterprise GitHub|$git_host"
+        set -a help_args -f "p|protocol|Clone protocol, either HTTPS or SSH|$protocol"
+        set -a help_args -f 'r|repos|The owner/name of the repository to clone'
+        set -a help_args -f "R|recursive|Recursive clone so submodules are initialized|$recursive"
 
         __dotfiles_help $help_args
     end
@@ -30,12 +34,16 @@ function git.clone -d "Clone repository to the layout used"
                 set base_directory $value
             case c cd
                 set cd_into true
+            case F full-checkout
+                set full_checkout "true"
             case H host
-                set -x git_host "$value"
+                set git_host "$value"
             case p protocol
-                set -x protocol (string lower $value)
+                set protocol (string lower $value)
             case r repos
-                set -x repos "$value"
+                set repos "$value"
+            case R recursive
+                set recursive "true"
                 # Common args
             case h help
                 ___usage
@@ -53,10 +61,17 @@ function git.clone -d "Clone repository to the layout used"
         __log debug "git_host : $git_host"
         __log debug "repo     : $repo"
         __log debug "protocol : $protocol"
-        test (string lower "$protocol") = ssh; and set -x git_url "git@$git_host:$repo"; or set -x git_url "https://$git_host/$repo"
+        test "$protocol" = "ssh"; and set -x git_url "git@$git_host:$repo"; or set -x git_url "https://$git_host/$repo"
         __log debug "git_url  : $git_url"
+        set -l local_dir "$base_directory/$git_host/$repo"
+        set -l git_clone_args clone
+        test "$full_checkout" != "true"; and set -a git_clone_args "--depth" "1"
+        test "$recursive" = "true"; and set -a git_clone_args "--recursive"
+        set -a git_clone_args "$git_url"
+        set -a git_clone_args "$local_dir"
 
-        git clone --depth 1 "$git_url" "$base_directory/$git_host/$repo"
+        __log debug "git $git_clone_args"
+        git $git_clone_args
 
         if test "$cd_into" = true
             cd "$base_directory/$git_host/$repo"
