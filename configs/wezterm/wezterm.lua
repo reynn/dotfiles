d5431fcfbdfe1036622428988dde424dbc96481c
Nic Patterson <nic@reynn.dev>
2022-07-30 22:20:14 -0700

    add wezterm config

---
+++ configs/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+return {
+	exit_behavior = "CloseOnCleanExit",
+	font = wezterm.font({
+		family = "Hasklug Nerd Font",
+	}),
+	font_size = 16.0,
+	freetype_load_target = "HorizontalLcd",
+	color_scheme = "kanagawabones",
+	initial_cols = 120, -- 80
+	initial_rows = 40, -- 24
+	window_decorations = "NONE",
+}
